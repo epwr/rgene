dfeb2b235c6892f94bbe6e0804d9e3ace86c5d8c
epwr <ericpower@outlook.com>
2021-10-19 12:04:48 -0700

    Removing tests for behaviour that is not wanted

--- spec/run_flag_handlers_tests.rb
+++ spec/run_flag_handlers_tests.rb
@@ -11,20 +11,9 @@ describe "Flag Handler" do
 
   it "should be able to list commands to create a simple ruby script." do
     config = {
-      gen_defaults: true,
-      make_directory: false,
       project: 'test_prj',
     }
-    expect(gen_commands(config, nil)[1]).to eq(["cat @simple_script.erb > test_prj.rb"])
-  end
-
-  it "should be able to list commands to create a project directory and simple script." do
-    config = {
-      gen_defaults: true,
-      make_directory: true,
-      project: 'test_prj',
-    }
-    expect(gen_commands(config, nil)[1]).to eq(["mkdir test_prj", "cat @simple_script.erb > test_prj/test_prj.rb"])
+    expect(gen_commands(config, nil)[1]).to eq(["cat @script_template.rb > test_prj.rb"])
   end
 
 
